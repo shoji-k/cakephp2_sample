ae2a18ffcc644e14f6d2f28d771b2b56c68954ba
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-13 12:35:49 +0900

    load Migration plugin

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -57,6 +57,7 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::loadAll(); // Loads all plugins at once
  * CakePlugin::load('DebugKit'); // Loads a single plugin named DebugKit
  */
+CakePlugin::load('Migrations');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By default CakePHP bundles two filters:
