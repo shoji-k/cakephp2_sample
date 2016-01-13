3e1a4c2a39d88eca2d82789c1b26cd428eb330b7
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2016-01-13 18:41:17 +0900

    create PostController and tests

--- Controller/PostsController.php
+++ Controller/PostsController.php
@@ -5,11 +5,50 @@ App::uses('AppController', 'Controller');
  */
 class PostsController extends AppController {
 
-    /**
-     * Scaffold
-     *
-     * @var mixed
-     */
-    public $scaffold;
+    public $components = ['Paginator', 'RequestHandler', 'Session'];
+
+    public $helpers = [
+        'Html' => ['className' => 'BoostCake.BoostCakeHtml'],
+        'Form' => ['className' => 'BoostCake.BoostCakeForm'],
+    ];
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+        // $this->Auth->allow('index');
+    }
+
+    public function index() {
+        // $this->Paginator->settings = $this->Post->getPaginateSettings($this->request->user_account);
+        $this->Paginator->settings = [
+            'limit' => 5,
+            'order' => ['Post.id' => 'desc']
+        ];
+        $this->set('posts', $this->Paginator->paginate());
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $current_user = ['username' => 'user1'];
+            // $current_user = $this->Auth->user();
+            // $this->request->data['Post']['author_id'] = $current_user['id'];
+
+            $this->Post->create($this->request->data);
+            if ($this->Post->save()) {
+                $this->Session->setFlash(
+                    __('新しい記事を受け付けました。'),
+                    'alert',
+                    ['plugin' => 'BoostCake', 'class' => 'alert-success']
+                );
+
+                return $this->redirect(['action' => 'index', 'user_account' => $current_user['username']]);
+            } else {
+                $this->Session->setFlash(
+                    __('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'),
+                    'alert',
+                    ['plugin' => 'BoostCake', 'class' => 'alert-success']
+                );
+            }
+        }
+    }
 
 }
--- Test/Case/Controller/PostsControllerTest.php
+++ Test/Case/Controller/PostsControllerTest.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('PostsController', 'Controller');
+App::uses('Fabricate', 'Fabricate.Lib');
 
 /**
  * PostsController Test Case
@@ -15,4 +16,47 @@ class PostsControllerTest extends ControllerTestCase {
         'app.post'
     );
 
+    public function setUp() {
+        parent::setUp();
+        $this->controller = $this->generate('Posts', [
+            //'components' => ['Paginator', 'Session', 'Auth'],
+            'components' => ['Paginator', 'Session'],
+            'models' => ['Post' => ['save']],
+            'methods' => ['redirect']
+        ]);
+        $this->controller->autoRender = false;
+    }
+
+    public function testIndexアクションではページングの結果がpostsにセットされること() {
+        $post = Fabricate::build('Post');
+        $this->controller->Paginator->expects($this->once())
+            ->method('paginate')->will($this->returnValue($post->data));
+        $vars = $this->testAction('/user/list', ['method' => 'get', 'return' => 'vars']);
+        $this->assertEquals($post->data, $vars['posts']);
+    }
+
+    public function testAddアクションで保存が失敗したときメッセージがセットされること() {
+        // $this->controller->Auth->expects($this->any())
+        //     ->method('loggedIn')->will($this->returnValue(true));
+        $this->controller->Post->expects($this->once())
+            ->method('save')->will($this->returnValue(false));
+        $this->controller->Session->expects($this->once())
+            ->method('setFlash')->with($this->equalTo('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'));
+
+        $this->testAction('/posts/add', ['method' => 'post', 'data' => ['title' => 'Title1', 'body' => 'Body1']]);
+    }
+
+    public function testAddアクションで保存が成功したときはメッセージがセットされ一覧表示にリダイレクトされること() {
+        // $this->controller->Auth->expects($this->any())
+        //     ->method('loggedIn')->will($this->returnValue(true));
+        // $this->controller->Auth->staticExpects($this->any())
+        //     ->method('user')->will($this->returnValue(['id' => '1', 'username' => 'user1']));
+        $this->controller->Post->expects($this->once())
+            ->method('save')->will($this->returnValue(true));
+        $this->controller->Session->expects($this->once())
+            ->method('setFlash')->with($this->equalTo('新しい記事を受け付けました。'));
+        $this->controller->expects($this->once())
+            ->method('redirect')->with($this->equalTo(['action' => 'index', 'user_account' => 'user1']));
+        $this->testAction('/posts/add', ['method' => 'post', 'data' => ['title' => 'Title1', 'body' => 'Body1']]);
+    }
 }
